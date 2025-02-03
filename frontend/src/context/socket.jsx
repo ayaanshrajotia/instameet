70bae19c8d4b58eb112be22eff2e8480afb47847
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2025-02-03 18:22:01 +0530

    fixed socket url in frontend

--- frontend/src/context/socket.jsx
+++ frontend/src/context/socket.jsx
@@ -3,13 +3,16 @@ import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
+const BASE_URL =
+    import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+
 export const useSocket = () => {
     return useContext(SocketContext);
 };
 
 // eslint-disable-next-line react/prop-types
 export const SocketProvider = ({ children }) => {
-    const socket = io("http://localhost:5001");
+    const socket = io(BASE_URL);
 
     return (
         <SocketContext.Provider value={{ socket }}>
