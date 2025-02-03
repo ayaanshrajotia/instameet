fd96200bd8dadf464d115489530b4c9920e4142a
Ayaansh Rajotia <86037577+ayaanshrajotia@users.noreply.github.com>
2025-02-03 18:09:07 +0530

    fixed env port error

--- backend/index.js
+++ backend/index.js
@@ -60,6 +60,6 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5001, () => {
-    console.log("Server is running on port 5001");
+server.listen(process.env.PORT, () => {
+    console.log("Server is running on port ", process.env.PORT);
 });
